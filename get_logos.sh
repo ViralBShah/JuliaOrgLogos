0039e2c7d2f4702422b4dab4ad799164fd22af2f
Sascha Mann <git@mail.saschamann.eu>
2020-11-20 19:36:46 +0100

    [CI] Automatically build logo collections

--- get_logos.sh
+++ get_logos.sh
@@ -6,5 +6,10 @@ GH_USER=`cat github_user`
 GH_TOKEN=`cat github_token`
 for o in `cat orgnames`; do
     echo "Downloading $o.png";
-    curl -s -u $GH_USER:$GH_TOKEN -L -o logos/$o.png https://github.com/$o.png
+    # During CI runs, use a GitHub App token rather than a Personal Access Token
+    if [[ $CI = "true" ]]; then
+        curl -s -H "Authorization: Bearer $GITHUB_TOKEN" -L -o logos/$o.png https://github.com/$o.png
+    else
+        curl -s -u $GH_USER:$GH_TOKEN -L -o logos/$o.png https://github.com/$o.png
+    fi
 done
